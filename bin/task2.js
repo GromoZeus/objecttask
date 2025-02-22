4a68208d64bd334158464295341f0758eb719c96
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-02-22 20:47:37 +0700

    task2.js added

---
+++ bin/task2.js
@@ -0,0 +1,30 @@
+// Реализуйте и экспортируйте функцию по умолчанию, которая принимает на вход цепь ДНК
+// и возвращает соответствующую цепь РНК (совершает транскрипцию РНК).
+// G -> C
+// C -> G
+// T -> A
+// A -> U
+// ДНК и РНК это последовательности нуклеотидов.
+
+const dnaToRna = (dna) => {
+  let rna = '';
+  const nucleotideDNA = {
+    G: 'C',
+    C: 'G',
+    T: 'A',
+    A: 'U',
+  };
+  dna.split('').forEach((nucleotide) => {
+    if (!nucleotideDNA[nucleotide]) {
+      rna = null;
+    } else if (rna !== null) {
+      rna += nucleotideDNA[nucleotide];
+    }
+  });
+  return rna;
+};
+
+console.log(dnaToRna('ACGTGGTCTTAA')); // 'UGCACCAGAAUU'
+console.log(dnaToRna('CCGTA')); // 'GGCAU'
+console.log(dnaToRna('')); // ''
+console.log(dnaToRna('ACNTG')); // null

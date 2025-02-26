86ea1739996b1e601c1d15ae85c506241fe34f29
Evgeniy Gammer <thiefmanmail@gmail.com>
2025-02-26 17:14:08 +0300

    task7.js added

---
+++ bin/task7.js
@@ -0,0 +1,27 @@
+// Реализуйте и экспортируйте по умолчанию функцию, которая принимает на
+//  вход массив (элементы которого — это объекты) и пары ключ-значение
+//  (тоже в виде объекта), а возвращает первый элемент исходного массива,
+//  значения которого соответствуют переданным парам (всем переданным).
+//  Если совпадений не было, то функция должна вернуть null.
+
+const findWhere = (arr, obj) => {
+  const keyStrObj = Object.entries(obj);
+  console.log(keyStrObj);
+  const result = arr.find((element) => {
+    const keyStrArr = Object.entries(element).flat().join();
+    return keyStrObj.every((item) => keyStrArr.includes(item.join()));
+  });
+  return result || null;
+};
+
+console.log(findWhere(
+  [
+    { title: 'Book of Fooos', author: 'FooBar', year: 1111 },
+    { title: 'Cymbeline', author: 'Shakespeare', year: 1611 },
+    { title: 'The Tempest', author: 'Shakespeare', year: 1611 },
+    { title: 'Book of Foos Barrrs', author: 'FooBar', year: 2222 },
+    { title: 'Still foooing', author: 'FooBar', year: 3333 },
+    { title: 'Happy Foo', author: 'FooBar', year: 4444 },
+  ],
+  { author: 'undefined', year: 1611 },
+));
